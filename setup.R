5bc7bbaa390c7391821f43026a3642898b58ee19
Rob Hyndman <robjhyndman@gmail.com>
2023-05-29 14:50:05 +1000

    Working on fr2

--- setup.R
+++ setup.R
@@ -12,9 +12,13 @@ options(
   ggplot2.discrete.colour = c("#D55E00", "#0072B2","#009E73", "#CC79A7", "#E69F00", "#56B4E9", "#F0E442"),
   ggplot2.discrete.fill = c("#D55E00", "#0072B2","#009E73", "#CC79A7", "#E69F00", "#56B4E9", "#F0E442")
 )
+knitr::opts_chunk$set(
+  dev.args = list(bg = grey(0.9), pointsize = 11)
+)
 
 # Font for graphics to be Fira Sans
 ggplot2::theme_set(ggplot2::theme_get() + ggplot2::theme(text = ggplot2::element_text(family = 'Fira Sans')))
 # Also in base R plots
-quartzFonts(firasans = c("Fira Sans Regular","Fira Sans Bold","Fira Sans Italic","Fira Sans Bold Italic"))
-par(family = "firasans")
+quartzFonts(
+  sans = c("Fira Sans Regular","Fira Sans Bold","Fira Sans Italic","Fira Sans Bold Italic")
+)
